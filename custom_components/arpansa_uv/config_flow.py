b8799cd7c595d939493bf0c8568a8e96afebcef3
Joshua Rich <joshua.rich@gmail.com>
2022-02-09 14:12:26 +1000

    Apparently can't use match case in prod HA? NFI why OptionsFlowHandler shows nothing in UI no matter what I try.

--- custom_components/arpansa_uv/config_flow.py
+++ custom_components/arpansa_uv/config_flow.py
@@ -16,9 +16,9 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import (
-    CONF_POLL_INTERVAL,
     CONF_LOCATIONS,
     CONF_NAME,
+    CONF_POLL_INTERVAL,
     DOMAIN,
     DEFAULT_SCAN_INTERVAL,
     DEFAULT_NAME,
@@ -39,31 +39,32 @@ async def build_schema(
     :param step: for which step we should build schema
     :return: Configuration schema with default parameters
     """ 
-    match step:
-        case "user":
-            arpansa = Arpansa()
-            session = async_get_clientsession(hass)
-            try:
-                await arpansa.fetchLatestMeasurements(session)
-            except ApiError as err:
-                raise CantConnect from err
-            locations = arpansa.getAllLocations()
-            schema = vol.Schema(
-                {   
-                    vol.Required(CONF_NAME,default=DEFAULT_NAME): cv.string,
-                    vol.Optional(CONF_LOCATIONS): cv.multi_select(locations),
+    # match step:
+    #     case "user":
+    if step == "user":
+        arpansa = Arpansa()
+        session = async_get_clientsession(hass)
+        try:
+            await arpansa.fetchLatestMeasurements(session)
+        except ApiError as err:
+            raise CantConnect from err
+        locations = arpansa.getAllLocations()
+        return vol.Schema(
+            {   
+                vol.Required(CONF_NAME,default=DEFAULT_NAME): cv.string,
+                vol.Optional(CONF_LOCATIONS): cv.multi_select(locations),
+            }
+        )
+        # case "init":
+    if step == "init":
+        return vol.Schema(
+                {
+                    vol.Required(CONF_POLL_INTERVAL, default=DEFAULT_SCAN_INTERVAL): vol.Range(min=1),
                 }
             )
-            return schema
-        case "init":
-            schema = vol.Schema(
-                    {
-                        vol.Required(CONF_POLL_INTERVAL, default=DEFAULT_SCAN_INTERVAL): vol.Range(min=1),
-                    }
-                )
-            return schema
-        case None:
-            raise UnknownStep
+        # case None:
+    if step == None:
+        raise UnknownStep
 
 
 async def validate_input(hass: HomeAssistant, data: dict[str, Any], step_id: str) -> dict[str, Any]:
@@ -72,29 +73,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any], step_id: str
     Data has the keys from build_schema() with values provided by the user.
     """
     # TODO validate the data can be used to set up a connection.
-    validated_input = {}
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    # hub = PlaceholderHub(data["poll_interval"])
-
-    # if not await hub.authenticate(data["username"], data["password"]):
-    #     raise InvalidAuth
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-    _LOGGER.info(f"Input to validate is {data}")
-
-    validated_input = data
-
-    # Return info that you want to store in the config entry.
-    return validated_input
+    return data
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -121,6 +100,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 )
             else:              
                 info = await validate_input(self.hass, data=user_input, step_id="user")
+                _LOGGER.debug(f"Creating config with data: {user_input}")
                 return self.async_create_entry(title=info[CONF_NAME], data=user_input)
             # return self.async_show_form(
             #     step_id="user", data_schema=config_schema, errors=errors
@@ -138,30 +118,33 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     
     @staticmethod
     @callback
-    def async_get_options_flow(config_entry):
+    def async_get_options_flow(
+        config_entry: config_entries.ConfigEntry,
+    ) -> OptionsFlowHandler:
         return OptionsFlowHandler(config_entry)        
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
+    """Handle a options flow for ARPANSA UV Values."""
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
         self.config_entry = config_entry
 
-    async def async_step_init(self, user_input=None):
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
         """Manage the options."""
-        errors = {}
-
         options_schema = await build_schema(
-            config_entry=None,
+            config_entry=self.config_entry,
             hass=self.hass,
             step = "init",
         )
 
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+        if user_input is None:
+            return self.async_show_form(
+                step_id="init", data_schema=options_schema)
 
-        return self.async_show_form(
-            step_id="init", data_schema=options_schema, errors=errors
-        )
+        _LOGGER.debug(f"Creating options with data: {user_input}")
+        return self.async_create_entry(title="", data=user_input)
 
 class CantConnect(HomeAssistantError):
     """Error to indicate failure to connect to API."""
